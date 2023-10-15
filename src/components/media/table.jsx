098283a239dbee392297ab0f710b6318b8ffb230
Osmin <osminv91@gmail.com>
2023-10-14 22:19:09 -0500

    commit table.jsx

--- src/components/media/table.jsx
+++ src/components/media/table.jsx
@@ -6,19 +6,23 @@ export function Table({media}){
             <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Date time of create</th>
+                    <th scope="col">sinopsis</th>
+                    <th scope="col">serial</th>
+                    <th scope="col">url</th>
+                    <th scope="col">image</th>
+                    <th scope="col">datetimecreate</th>
                     <th scope="col">Opciones</th>
                 </tr>
             </thead>
             <tbody>{
-                media.map((person,index) => (<Fragment>
+                media.map((medi,index) => (<Fragment>
                     <tr key={index}>
                         <th scope="row">{index + 1}</th>
-                        <td>{person.name}</td>
-                        <td>{person.status ? 'Activo':'Inactivo'}</td>
-                        <td>{person.datetimecreate}</td>
+                        <td>{medi.sinopsis}</td>
+                        <td>{medi.serial}</td>
+                        <td>{medi.url}</td>
+                        <td>{medi.image}</td>
+                        <td>{medi.datetimecreate}</td>
                         <td>
                             <button type="button" className="btn btn-danger">Borrar</button>
                             <button type="button" className="btn btn-warning">Editar</button>
